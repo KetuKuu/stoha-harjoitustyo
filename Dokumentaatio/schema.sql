92f3f0209177c486f040b786439d09a0ac10b751
kety <kety.kuusaru@helsinki.fi>
2024-04-21 21:15:24 +0300

    Add SQL schema

--- Dokumentaatio/schema.sql
+++ Dokumentaatio/schema.sql
@@ -1,13 +1,22 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, 
+    username TEXT UNIQUE, 
+    password TEXT
+);
+
 CREATE TABLE polls (
     id SERIAL PRIMARY KEY,
     topic TEXT,
     created_at TIMESTAMP
 );
+
 CREATE TABLE choices (
     id SERIAL PRIMARY KEY,
     poll_id INTEGER REFERENCES polls,
     choice TEXT
 );
+
 CREATE TABLE answers (
     id SERIAL PRIMARY KEY,
     choice_id INTEGER REFERENCES choices,
