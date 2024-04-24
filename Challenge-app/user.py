b7eab4a99cfbfc8df28c097fae3df43a19f5d605
kety <kety.kuusaru@helsinki.fi>
2024-04-24 13:42:32 +0300

    User registration works correctly

--- Challenge-app/user.py
+++ Challenge-app/user.py
@@ -5,38 +5,47 @@ from sqlalchemy.sql import text
 from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
+from datetime import datetime
+
+
+def login (username, password):
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result_user =db.session.execute (text(sql), {"username":username})
+    user = result_user.fetchone()
+
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["username"] = username 
+            session["user_id"] = user.id
+            return True
+        else:
+            return False
 
 
 
-def get_user(username,password):
+def register (username, password):
 
     sql = "SELECT id, password FROM users WHERE username=:username"
     result = db.session.execute(text(sql), {"username":username})
     user = result.fetchone()
-    if not user:
-        hash_value = generate_password_hash(password)
-        # Insert new user to the database
+    if user:
+        return False
+
+    hash_value = generate_password_hash(password)
+    try:
         sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
         db.session.execute(text(sql), {"username":username, "password":hash_value})
         db.session.commit()
-        
-        #return redirect("/loging")
-        return "new_user_created" 
-    
-    else:
-        hash_value = user.password
-        if check_password_hash(hash_value, password):    
-            session["username"] = username
-            print("session[\"username\"]: ", session["username"])
-            return "login_successful"
-        else:
-            error_message = "Incorrect username or password"
-            #return render_template("loging.html", error_message=error_message)
-            return "login_failed"
-
-
+       
+    except:
+        return False
+    return login(username, password)
 
 def logout():
     del session["username"]
     #return redirect("/loging")
     return redirect("/Frontpage")
+
+
