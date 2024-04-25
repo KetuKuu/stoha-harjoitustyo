e2f0e9e454fe75fcf9b7b1d88fa4a19f064b7865
kety <kety.kuusaru@helsinki.fi>
2024-04-25 21:57:57 +0300

    File config

--- Challenge-app/app.py
+++ Challenge-app/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
-from os import getenv
+from os import getenv,makedirs
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
 
 app = Flask(__name__)
 app.debug = True
+
+
+# Määrittele tiedostojen tallennuspolku ja varmista, että kansio on olemassa
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
 import routes
 
