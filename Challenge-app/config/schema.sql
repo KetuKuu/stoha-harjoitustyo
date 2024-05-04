03e61bda24ba950cb0981f57a54cc3a121b8b05c
kety <kety.kuusaru@helsinki.fi>
2024-05-04 22:18:13 +0300

    card_media added

--- Challenge-app/config/schema.sql
+++ Challenge-app/config/schema.sql
@@ -21,22 +21,36 @@ CREATE TABLE answers (
     id SERIAL PRIMARY KEY,
     choice_id INTEGER REFERENCES choices,
     sent_at TIMESTAMP
-);
 
-CREATE TABLE cards (                                                           
+);
+CREATE TABLE cards (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     image_url TEXT,
     region TEXT,
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT NOW(),
+    user_id INTEGER
+
+);
+
+CREATE TABLE card_media (
+    id SERIAL PRIMARY KEY,
+    card_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    image_filename TEXT NOT NULL,
+    description TEXT,
+    FOREIGN KEY (card_id) REFERENCES cards(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL,
+    title TEXT NOT NULL,
+
 );
 
 CREATE TABLE completions (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
     region TEXT,
-    completion_date TIMESTAMP DEFAULT NOW()
+    completion_date TIMESTAMP DEFAULT NOW(),
     card_id INTEGER REFERENCES cards(id)
 );
 
